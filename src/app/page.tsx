11c90ae3cc8f2fcd37a52bf481b86849b22ba429
fabrown40 <fabrown40@gmail.com>
2024-04-17 13:29:59 +0100

    useEffect removed on app-page-home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,6 @@ import DetailsHouse from "./ui/detailsHouse";
 
 export default function Home() {
 
-  useEffect(()=>{
-    window.location.reload()
-  })
 
   return (
     <main className="flex min-h-screen   flex-col items-center ">
